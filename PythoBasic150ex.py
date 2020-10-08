90370076b1b37e12b3de4a7c1d72317be6672800
Varqina <flisu2@gmail.com>
2020-10-08 23:22:59 +0200

    resolved and validated for provided string

--- PythoBasic150ex.py
+++ PythoBasic150ex.py
@@ -10,8 +10,18 @@ Twinkle, twinkle, little star,
 	How I wonder what you are
 
 """
+
+
 def task1(string):
-    #if star, appeared make /n and \t
-    #if are! appeared make /n and 2x \t
-    #if high, appeared make /n and 2x \t
-    #if sky. appeared make /n
+    # if star, appeared make /n and \t
+    string = string.replace("star,", "star,\n\t")
+    # if are! appeared make /n and 2x \t
+    string = string.replace("are!", "are!\n\t\t")
+    # if high, appeared make /n and 2x \t
+    string = string.replace("high,", "high,\n\t\t")
+    # if sky. appeared make /n
+    string = string.replace("sky.", "sky.\n")
+    return string
+
+sString = "Twinkle, twinkle, little star,How I wonder what you are!Up above the world so high,Like a diamond in the sky.Twinkle, twinkle, little star, How I wonder what you are"
+#print(task1(sString))
