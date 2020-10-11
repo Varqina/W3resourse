934abe85f47171a80e2d756898c7a1bc2da8d039
Varqina <flisu2@gmail.com>
2020-10-11 22:56:35 +0200

    Task 5

--- PythoBasic150ex.py
+++ PythoBasic150ex.py
@@ -59,3 +59,13 @@ def task4(r):
 
 print(task4(1.1))
 
+""" Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them."""
+def task5(name, surname):
+    finalName=""
+    for letter in surname+name:
+        finalName+=letter+" "
+    print(finalName)
+
+task5("Piotr", "Flis")
+
+
